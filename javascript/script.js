c291ee86a5808726ec197b878e8baefe471c06f2
thanuja <pagadalathanujaa@gmail.com>
2026-02-27 16:06:38 +0530

    js variables,data types

---
+++ javascript/script.js
@@ -0,0 +1,81 @@
+// console.log("Hello World");
+// //reinitialize and redeclare
+// //global scope
+// var a;
+// a=10;
+// var a;
+// a=20;
+// //reinitialize
+// let b=10;  //block scope
+// //le;  //block scope
+// console.log(a);
+// console.log(b);
+// console.log(c);t b;
+// b=20;
+// //can't redeclare or reinitialize
+// const c=30
+
+//Data types
+//primitive
+// var num=10;  //number
+// var str="thanu"; //`` '' ""  //string
+// var bool=true;  //boolean
+// var un;//undefined
+// var nu=null;  //object
+// var big=123456789079n;  //bigint
+// var symbol=Symbol('li'); //symbol
+
+// console.log(typeof num)
+// console.log(typeof str)
+// console.log(typeof bool)
+// console.log(typeof un)
+// console.log(typeof nu)
+// console.log(typeof big)
+// console.log(typeof symbol)
+
+// var arr=[1,2,3,4,5] //array
+// console.log(typeof arr);
+
+// var Obj={
+//     name:"revanth",
+//     dept:"ds"
+// }
+// console.log(Obj);
+// console.log(typeof Obj)
+
+// //arithmatic operator 
+// var a=10;
+// var b="2";
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a%b)
+// console.log(a*b)
+// console.log(a**b)
+// //relational operators
+//  var a=10
+// var b=10
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a!=b)
+// console.log(a!==b)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a<=b)
+// console.log(a>=b)
+
+// //logical
+// var a=true;
+// var b=true;
+// console.log(a&&b)
+// console.log(a||b)
+// console.log(!b)
+
+
+
+
+
+
+
+
+
+
